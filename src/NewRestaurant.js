43fd9f7eda37de382d8abe2911fa274a61398ad3
aurimas-darguzis <aurimas@darguzis.com>
2017-04-18 11:51:17 +0100

    displaying content for logged in users and ability to submit a restaurant

--- src/NewRestaurant.js
+++ src/NewRestaurant.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react'
+import { database } from './firebase'
 import './NewRestaurant.css'
 
 class NewRestaurant extends Component {
@@ -8,11 +9,13 @@ class NewRestaurant extends Component {
       name: ''
     }
 
+    this.restaurantsRef = database.ref('/restaurants')
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit (event) {
     event.preventDefault()
+    this.restaurantsRef.push({ name: this.state.name })
   }
 
   render () {
@@ -25,7 +28,7 @@ class NewRestaurant extends Component {
         <input
           type='text'
           value={name}
-          placeholder='Name of Fine Establishment'
+          placeholder='Name of Fine Establishment'  
           onChange={(event) => this.setState({ name: event.target.value })}
         />
         <button
